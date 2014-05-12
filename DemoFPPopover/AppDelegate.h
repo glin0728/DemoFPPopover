0f7bab6efb601547a0c3ea1993324f2b44fbbc45
glin <glin0728@163.com>
2014-05-12 21:55:30 +0800

    Initial Commit

---
+++ DemoFPPopover/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoFPPopover
+//
+//  Created by TopCoder on 5/12/14.
+//  Copyright (c) 2014 TCer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoFPPopoverTests/DemoFPPopoverTests.m
@@ -0,0 +1,34 @@
+//
+//  DemoFPPopoverTests.m
+//  DemoFPPopoverTests
+//
+//  Created by TopCoder on 5/12/14.
+//  Copyright (c) 2014 TCer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DemoFPPopoverTests : XCTestCase
+
+@end
+
+@implementation DemoFPPopoverTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
